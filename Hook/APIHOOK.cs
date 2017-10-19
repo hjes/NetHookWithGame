48b970f3046fe461b410b6b814dfb093d9b7d4e8
桃花依旧笑春风 <1605000001@qq.com>
2017-10-19 16:25:03 +0800

    FirstCommit
    FirstCommit

---
+++ Hook/APIHOOK.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+ 
+
+namespace Hook
+{
+    class APIHOOK
+    {
+        #region Api声明
+        [DllImport("kernel32.dll", EntryPoint = "GetModuleHandleA", SetLastError = true, CharSet = CharSet.Ansi, ThrowOnUnmappableChar = true)]
+        public static extern IntPtr GetModuleHandleA(String lpModuleName);
+        [DllImport("kernel32.dll", EntryPoint = "GetModuleHandleW", SetLastError = true, CharSet = CharSet.Unicode, ThrowOnUnmappableChar = true)]
+        public static extern IntPtr GetModuleHandleW(String lpModuleName);
+        [DllImport("Kernel32.dll")]
+        static extern bool VirtualProtect(
+            IntPtr lpAddress,
+            int dwSize,
+            int flNewProtect,
+            ref int lpflOldProtect
+            );
+        [DllImport("Kernel32.dll", EntryPoint = "lstrcpynA", CharSet = CharSet.Ansi)]
+        static extern IntPtr lstrcpyn(
+            byte[] lpString1,
+            byte[] lpString2,
+            int iMaxLength
+            );
+        [DllImport("Kernel32.dll")]
+        static extern IntPtr GetProcAddress(
+            IntPtr hModule,
+            string lpProcName
+            );
+        [DllImport("Kernel32.dll")]
+        static extern bool FreeLibrary(
+            IntPtr hModule
+            );
+        #endregion
+        #region 常量定义表
+        int PAGE_EXECUTE_READWRITE = 0x40;
+        #endregion
+        #region 变量表
+        public IntPtr ProcAddress;
+        int lpflOldProtect = 0;
+        byte[] OldEntry = new byte[5];
+        byte[] NewEntry = new byte[5];
+        IntPtr OldAddress;
+        #endregion
+        public APIHOOK() { }
+        public APIHOOK(string ModuleName, string ProcName, IntPtr lpAddress)
+        {
+            Install(ModuleName, ProcName, lpAddress);
+        }
+        public bool Install(string ModuleName, string ProcName, IntPtr lpAddress)
+        {
+            IntPtr hModule = GetModuleHandleA(ModuleName); //取模块句柄   
+            if (hModule == IntPtr.Zero)
+            {
+                Console.WriteLine("您的机子因Sock问题无法运行此软件");
+                return false;
+            }
+            ProcAddress = GetProcAddress(hModule, ProcName); //取入口地址   
+            if (ProcAddress == IntPtr.Zero) return false;
+            if (!VirtualProtect(ProcAddress, 5, PAGE_EXECUTE_READWRITE, ref lpflOldProtect)) return false; //修改内存属性   
+            Marshal.Copy(ProcAddress, OldEntry, 0, 5); //读取前5字节   
+            NewEntry = AddBytes(new byte[1] { 233 }, BitConverter.GetBytes((Int32)((Int32)lpAddress - (Int32)ProcAddress - 5))); //计算新入口跳转   
+            Marshal.Copy(NewEntry, 0, ProcAddress, 5); //写入前5字节   
+            OldEntry = AddBytes(OldEntry, new byte[5] { 233, 0, 0, 0, 0 });
+            OldAddress = lstrcpyn(OldEntry, OldEntry, 0); //取变量指针   
+            Marshal.Copy(BitConverter.GetBytes((double)((Int32)ProcAddress - (Int32)OldAddress - 5)), 0, (IntPtr)(OldAddress.ToInt32() + 6), 4); //保存JMP   
+            FreeLibrary(hModule); //释放模块句柄   
+            return true;
+        }
+        public void Suspend()
+        {
+            Marshal.Copy(OldEntry, 0, ProcAddress, 5);
+        }
+        public void Continue()
+        {
+            Marshal.Copy(NewEntry, 0, ProcAddress, 5);
+        }
+        public bool Uninstall()
+        {
+            if (ProcAddress == IntPtr.Zero) return false;
+            Marshal.Copy(OldEntry, 0, ProcAddress, 5);
+            ProcAddress = IntPtr.Zero;
+            return true;
+        }
+        static byte[] AddBytes(byte[] a, byte[] b)
+        {
+            ArrayList retArray = new ArrayList();
+            for (int i = 0; i < a.Length; i++)
+            {
+                retArray.Add(a[i]);
+            }
+            for (int i = 0; i < b.Length; i++)
+            {
+                retArray.Add(b[i]);
+            }
+            return (byte[])retArray.ToArray(typeof(byte));
+        }
+    }
+}
---
+++ Hook/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Hook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("ws2_32.dll")]
+        public static extern int recv(int s1, IntPtr buf1, int len1, int flag1);
+        [DllImport("ws2_32.dll")]
+        public static extern int send(int s, byte[] buf, int len, int flag);
+        APIHOOK send_Hook = new APIHOOK();
+        APIHOOK recv_Hook = new APIHOOK();
+        public delegate int sendCallback(int s, IntPtr buf, int len, int flag);
+        public delegate int recvCallback(int s1, IntPtr buf1, int len1, int flag1);
+        private static Delegate deleg;
+        private static Delegate delegR;
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://baidu.com");
+            deleg = new sendCallback(sendProc);
+            delegR = new recvCallback(recvProc);
+            send_Hook.Install("ws2_32.dll", "send", Marshal.GetFunctionPointerForDelegate(deleg));
+            recv_Hook.Install("ws2_32.dll", "recv", Marshal.GetFunctionPointerForDelegate(delegR));
+        }
+        int sendProc(int s, IntPtr buf, int len, int flag)
+        {
+           
+            int ret = 0;
+            byte[] buffer = new byte[len];
+            Marshal.Copy(buf, buffer, 0, len); //读封包数据 
+            string sendstr = Encoding.UTF8.GetString(buffer);
+            Console.WriteLine("Send:"+sendstr);
+            send_Hook.Suspend(); //暂停拦截，转交系统调用
+            ret = send(s, buffer, len, flag); //发送数据，此处可进行拦截
+            send_Hook.Continue(); //恢复HOOK  
+            return ret;
+        }
+        int recvProc(int s2, IntPtr buf2, int len2, int flag2)
+        {
+            int ret = 0;
+            byte[] buffer1 = new byte[len2];
+            recv_Hook.Suspend(); //暂停拦截，转交系统调用 
+            ret = recv(s2, buf2, len2, flag2); //接收数据，此处可进行拦截
+            recv_Hook.Continue(); //恢复HOOK
+            if (ret == -1) //SOCKET_ERROR
+                return ret;
+            byte[] buffer2 = new byte[ret];
+            Marshal.Copy(buf2, buffer2, 0, buffer2.Length); //读封包数据 
+            Console.WriteLine("Recv:"+Encoding.UTF8.GetString(buffer2));
+            return ret;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://baidu.com");
+        }
+    }
+}
